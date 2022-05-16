d6dbd4b8fe199d11d60e0aefa5360374c1412ca3
Your Name <you@example.com>
2022-05-16 15:03:13 -0300

    configuring tailwind and importing assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 const App = () => {
   return (
-    <div className="App">
-     <h1 className='text-2xl' >App Works</h1>
+    <div className="m-auto antialised font-sans font-serif font-mono text-center">
+      <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl ">
+        <h1 className='text-blue-300'>App Works</h1>
+      </header>
     </div>
   );
 }
