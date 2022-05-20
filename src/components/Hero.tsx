ddff9237f3a39a518019b8e09ced366b8fe34c02
Bruno <bruno.sdsoares@gmail.com>
2022-05-19 23:21:57 -0300

    styling circle score

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,35 @@
 import placeholder from '../assets/Layer1.jpg'
 
-const Hero = () => (
-  <header className="min-h-screen">
+const Hero = ({ title = "Avengers Endgame", score = 10 }) => {
+ const getBorderColor = () => {
+  if (score >= 7) {
+     return 'border-green-400';
+  } else if (score > 4 && score < 7) {
+    return "border-yellow-400";
+  } else {
+    return "border-red-400"
+  }
+
+ }
+
+ return (
+ <header className="relative min-h-screen">
     <img
       className="object-cover object-center h-full w-full"
       src={placeholder}
       alt="capa do filme os vingadores em destaque"
     />
+    <article className="absolute bottom-0 mb-64 px-8">
+      <p className="text-3xl">Assista agora:</p>
+      <h2 className="text-6xl font-bold">{title}</h2>
+      <p className="text-base">
+        Nota <span className={`inline-block mx-2 py-2 px-3 border-4 bg-black bg-opacity-75 rounded-full ${getBorderColor()}`}>{score}</span>
+      </p>
+      <button className="text-base">Assistir</button>
+      <button className="text-base">Minha Lista</button>
+    </article>
   </header>
-);
+ );
+};
 
 export default Hero;
