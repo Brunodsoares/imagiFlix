965050d8283317cf7855d0bf15b14a4261a58076
Bruno <bruno.sdsoares@gmail.com>
2022-05-23 22:27:32 -0300

    carousel implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
+import Carousel from "./components/Carousel";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
   return (
     <div className="m-auto bg-black text-white antialised font-sans">
       <Hero />
       <NavBar />
+      <Carousel />
     </div>
   );
 };
---
+++ src/data/mock.tsx
@@ -0,0 +1,44 @@
+import cover1 from "../assets/Layer3.jpg";
+import cover2 from "../assets/Layer4.jpg";
+import cover3 from "../assets/Layer5.jpg";
+
+const mockData = [
+  {
+    title: "Avengers",
+    cover: cover1,
+  },
+  {
+    title: "Missão Impossível",
+    cover: cover2,
+  },
+  {
+    title: "Mega Tubarão",
+    cover: cover3,
+  },
+  {
+    title: "Avengers",
+    cover: cover1,
+  },
+  {
+    title: "Missão Impossível",
+    cover: cover2,
+  },
+  {
+    title: "Mega Tubarão",
+    cover: cover3,
+  },
+  {
+    title: "Avengers",
+    cover: cover1,
+  },
+  {
+    title: "Missão Impossível",
+    cover: cover2,
+  },
+  {
+    title: "Mega Tubarão",
+    cover: cover3,
+  },
+];
+
+export default mockData;
