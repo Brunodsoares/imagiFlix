7c3912521d03350b76e0456d2561436b54363046
Bruno <bruno.sdsoares@gmail.com>
2022-05-27 23:51:55 -0300

    applying score on the cover of movies

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -1,8 +1,10 @@
-import { Movie } from "../data/mock";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { faCirclePlay } from "@fortawesome/free-regular-svg-icons";
+
+import Score from "./Score";
 
 import './Poster.css'
+import { Movie } from "../data/mock";
 
 const Poster = ({ cover, title, score }: Movie, index: number) => (
   <article
@@ -10,9 +12,10 @@ const Poster = ({ cover, title, score }: Movie, index: number) => (
     key={index}
   >
     <img src={cover} alt={title} />
-    <div className="poster absolute inset-0 w-full h-full grid place-items-center bg-black bg-opacity-75 transition-all duration-500 ease-in-out opacity-0">
+    <div className="poster cursor-pointer absolute inset-0 w-full h-full py-8 px-4 grid place-items-center bg-black bg-opacity-75 transition-all duration-500 ease-in-out opacity-0">
       <FontAwesomeIcon icon={faCirclePlay} size="5x" />
       <h2 className="text-2xl">{title}</h2>
+      <Score value={score} />
     </div>
   </article>
 );
