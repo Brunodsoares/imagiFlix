0e5d7bc708c1db12509a66da6cd8db7c328bb8ec
Bruno <bruno.sdsoares@gmail.com>
2022-06-02 23:17:17 -0300

    applying loading logic

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const App = () => {
 
   const [ movies, setMovies ] = useState();
   const [ series, setSeries ] = useState();
+  const [ loading, setLoading ] = useState(true);
   
   useEffect(() => {
     const fetchData = async () => {
@@ -25,8 +26,10 @@ const App = () => {
       const series = await fetch(`${URL}/discover/tv${APISTRING}&sort_by=popularity.desc`);
       const seriesData = await series.json();
       setSeries(seriesData);
+      
+      setLoading(false);
     };
-
+    
     fetchData();
   }, []);
 
@@ -44,12 +47,17 @@ const App = () => {
 
   return (
     <div className="m-auto  antialised font-sans bg-black text-white">
-      <Hero {...getFeaturedMovie()} />
-      <NavBar />
-      <Carousel title="Filmes Populares" data={getMovieList()} />
-      <Carousel title="Séries Populares" data={series?.results} />
-      <Carousel title="Placeholder" />
-      <Footer />
+      {loading && <h1>LOADING</h1>}
+      {!loading && (
+        <>
+          <Hero {...getFeaturedMovie()} />
+          <NavBar />
+          <Carousel title="Filmes Populares" data={getMovieList()} />
+          <Carousel title="Séries Populares" data={series?.results} />
+          <Carousel title="Placeholder" />
+          <Footer />
+        </>
+      )}
     </div>
   );
 };
